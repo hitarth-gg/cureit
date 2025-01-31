352c926b7a3019478927ed2503c8249e7210b545
hitarth <goforhitarth@gmail.com>
2025-01-31 14:30:30 +0530

    Frontend | Added timer to resend email button

--- frontend/src/pages/AccountVerification.jsx
+++ frontend/src/pages/AccountVerification.jsx
@@ -1,13 +1,40 @@
 import { Button, Code } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function AccountVerification() {
   const location = useLocation();
-  const email = location.state.email || "undefined email";
+  const email = location?.state?.email || "undefined email";
+  // add 1.5min timer to resend email
+
+  const [timer, setTimer] = useState(90); // Initial timer value in seconds
+  const [isTimerActive, setIsTimerActive] = useState(true);
+
+  useEffect(() => {
+    let interval = null;
+    if (isTimerActive && timer > 0) {
+      interval = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+    } else if (timer === 0) {
+      setIsTimerActive(false);
+    }
+    return () => clearInterval(interval); // Cleanup interval
+  }, [timer, isTimerActive]);
+
+  const handleResend = () => {
+    // Trigger email resend logic here
+    console.log("Resend email triggered!");
+
+    // Start timer
+    setTimer(90);
+    setIsTimerActive(true);
+  };
+
   return (
     <div className="flex h-[94svh] w-full items-center justify-center bg-[#f7f8fa] font-noto font-medium">
-      <div className="flex flex-col m-4 items-center justify-center gap-y-4 rounded-lg bg-white p-8 drop-shadow-lg">
-        <div className="mb-4 rounded-sm border text-center border-green-400 bg-green-200 px-3 py-1 text-lg font-semibold text-green-900">
+      <div className="m-4 flex flex-col items-center justify-center gap-y-4 rounded-lg bg-white p-8 drop-shadow-lg">
+        <div className="mb-4 rounded-sm border border-green-400 bg-green-200 px-3 py-1 text-center text-lg font-semibold text-green-900">
           Please verify your email
         </div>
         <div>You're almost there! We sent an email to</div>
@@ -21,8 +48,12 @@ function AccountVerification() {
         <div className="flex flex-col items-center text-sm">
           <div>Still can't find the email?</div>
           <div className="my-1">
-            <Button color="iris"  onClick={() => console.log("Resend email")}>
-              Resend email
+            <Button
+              color="iris"
+              disabled={isTimerActive}
+              onClick={handleResend}
+            >
+              {isTimerActive ? `Resend email in ${timer}s` : `Resend email`}
             </Button>
           </div>
         </div>
