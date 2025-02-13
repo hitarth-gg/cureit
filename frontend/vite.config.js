81cc185c69338ef5d9466eedb8fe565b9e396471
hitarth <goforhitarth@gmail.com>
2025-02-13 23:45:03 +0530

    github static page template added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/codeforces-explorer",
+  base: "/cureit",
   plugins: [react(), eslint()],
 });
