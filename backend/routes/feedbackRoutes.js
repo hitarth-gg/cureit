b56e390487c78946bbda052e8d510360c3024f8f
aryamagarwal <mailaryam1000@gmail.com>
2025-02-14 13:10:04 +0530

    tags feature linked with backend

--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,22 +1,36 @@
-
 const express = require("express");
 const router = express.Router();
 const supabase = require("../config/supabaseClient");
-
 router.post("/add/:id", async (req, res) => {
   const { id } = req.params;
   console.log("body: ", req.body)
   const { feedback, doctorId } = req.body;
-  const tags = ["useless", "fraud"];
+  let tag = null
+  try {
+    const response = await fetch("https://hackofiesta-2.onrender.com/classify/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ feedback }),
+    })
+    if (!response.ok) {
+      throw new Error("Error in classifying feedback")
+    }
+    const data = await response.json()
+    tag = data.tag || []
+  }
+  catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: error.message });
+  }
   const { data: currentTags, error: currentTagsError } = await supabase.from('doctors').select('tags').eq('id', doctorId).single();
   if (currentTagsError) {
     return res.status(400).json({ error: currentTagsError.message });
   }
   console.log("Current tags: ", currentTags);
   let tagMap = currentTags?.tags || {};
-  tags.forEach((tag) => {
-    tagMap[tag] = (tagMap[tag] || 0) + 1;
-  });
+  tagMap[tag] = (tagMap[tag] || 0) + 1;
   const { data: updateDoctor, error: updatDoctorError } = await supabase.from('doctors').update({ tags: tagMap }).eq('id', doctorId).select('*').single();
   if (updatDoctorError) {
     return res.status(400).json({ error: updatDoctorError.message });
@@ -33,5 +47,4 @@ router.post("/add/:id", async (req, res) => {
   console.log("Feedback added successfully");
   return res.status(201).json(data);
 });
-
 module.exports = router;
