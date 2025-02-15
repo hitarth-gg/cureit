eb0b0b1848557bf0cf8128d67beb643412cefed2
Priyanshu-2005 <pr.agarwal2005+github@gmail.com>
2025-02-15 10:55:22 +0530

    Changes made

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -40,7 +40,7 @@ function SignUpPage() {
       phone: phoneNumber,
       options: {
         data: { display_name: name, phone: phoneNumber }, // Store display_name in metadata
-        emailRedirectTo: `${frontend_base_url}cureit/user/dashboard/`, // Redirect URL after confirmation
+        emailRedirectTo: `${frontend_base_url}cureit/login/`, // Redirect URL after confirmation
       },
     });
 
